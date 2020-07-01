bea1bec191ac299db0821a8084000891320d95cf
jacob-gomes <gmsjcb@yahoo.co.in>
2020-07-01 14:26:17 +0530

    swagger added

--- src/main/java/com/sapient/App.java
+++ src/main/java/com/sapient/App.java
@@ -1,13 +1,25 @@
 package com.sapient;
 
-/**
- * Hello world!
- *
- */
+
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+	public static void main( String[] args ){
+    	SpringApplication.run(App.class, args);
     }
+	
+	@PostConstruct
+	public void initTimeZone() {
+		TimeZone.setDefault(TimeZone.getTimeZone("GMT"));
+	}
 }
