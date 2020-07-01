eea6e3a511792f0bdaf2f6e34539a9e0747d150f
jacob-gomes <gmsjcb@yahoo.co.in>
2020-07-01 15:51:08 +0530

    .

--- src/main/java/com/sapient/constant/Constant.java
+++ src/main/java/com/sapient/constant/Constant.java
@@ -2,6 +2,6 @@ package com.sapient.constant;
 
 public class Constant {
 
-	public static final String GET_COUNTRY_URL = "https://apiv2.apifootball.com/?action=get_countries&APIkey=9bb66184e0c8145284fd2cc0f7b914ada57b4e8fb2e4d6d586adcc27c257a978";
+	public static final String GET_COUNTRY_URL = "https://apiv2.apifootball.com/?action=get_countries&APIkey=9bb66184e0c8145384fd2cc0f7b914ada57b4e8fd2e4d6d586adcc27c257a978";
 
 }
